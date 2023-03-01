547c8fcb2b54f42465789fe5c214ad5d57407fea
Fabio Fiorita <fabiolfp@gmail.com>
2023-03-01 17:19:22 -0300

    Challenge Day 2

--- src/jvmMain/kotlin/Main.kt
+++ src/jvmMain/kotlin/Main.kt
@@ -10,18 +10,25 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.toComposeImageBitmap
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import java.io.ByteArrayOutputStream
+import java.net.HttpURLConnection
+import java.net.URL
+import javax.imageio.ImageIO
 
 @Composable
 @Preview
 fun App() {
+    val image = "https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_FMjpg_UX674_.jpg"
     MaterialTheme {
         Column {
             Text("You", fontSize = 30.sp)
             Image(
-                painter = painterResource("you.png"),
-                contentDescription = "You Poster",
+                bitmap = image.loadImageBitmap(),
+                contentDescription = "Image Description",
                 modifier = Modifier.height(300.dp)
             )
             Text("Nota: 7,7", fontSize = 20.sp)
@@ -31,7 +38,25 @@ fun App() {
 }
 
 fun main() = application {
-    Window(onCloseRequest = ::exitApplication) {
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "IMDB",
+    ) {
         App()
     }
+}
+
+fun String.loadImageBitmap(): ImageBitmap {
+    val url = URL(this)
+    val connection = url.openConnection() as HttpURLConnection
+    connection.connect()
+
+    val inputStream = connection.inputStream
+    val bufferedImage = ImageIO.read(inputStream)
+
+    val stream = ByteArrayOutputStream()
+    ImageIO.write(bufferedImage, "png", stream)
+    val byteArray = stream.toByteArray()
+
+    return org.jetbrains.skia.Image.makeFromEncoded(byteArray).toComposeImageBitmap()
 }
