b17cb7e4aeff6b572c92a4ed17f45095130b7a08
Fabio Fiorita <fabiolfp@gmail.com>
2023-03-01 16:00:46 -0300

    Challenge Day 1

--- src/jvmMain/kotlin/Main.kt
+++ src/jvmMain/kotlin/Main.kt
@@ -1,25 +1,31 @@
 import androidx.compose.material.MaterialTheme
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.material.Button
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 
 @Composable
 @Preview
 fun App() {
-    var text by remember { mutableStateOf("Hello, World!") }
-
     MaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop!"
-        }) {
-            Text(text)
+        Column {
+            Text("You", fontSize = 30.sp)
+            Image(
+                painter = painterResource("you.png"),
+                contentDescription = "You Poster",
+                modifier = Modifier.height(300.dp)
+            )
+            Text("Nota: 7,7", fontSize = 20.sp)
+            Text("Ano: 2018", fontSize = 20.sp)
         }
     }
 }
